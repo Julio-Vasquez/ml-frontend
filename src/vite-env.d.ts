56ce823a0cebb3c2929bc90473b0d8544955d04e
Julio-Vasquez <jualvalitube@gmail.com>
2023-03-19 16:47:28 -0500

    feat(config): prettier, env, and clean project

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_BASE_URL_API: string
+    readonly VITE_TIMEOUT_API: number
+    readonly VITE_PORT: number
+    readonly VITE_PREFIX: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
